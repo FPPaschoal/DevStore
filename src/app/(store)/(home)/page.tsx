e9adf4f06db5f811142de2933558fc2cb9facd09
Felipe Paschoal <112896945+FPPaschoal@users.noreply.github.com>
2024-04-01 11:32:07 -0300

    feat: adicionando o prettier

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,23 +1,23 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+    <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
       <Link
         href="/"
-        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+        className="group relative col-span-6 row-span-6 flex items-center justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
-          src={'/moletom-ai-side.png'}
+          src={"/moletom-ai-side.png"}
           width={860}
           height={860}
           quality={100}
           alt=""
-          className="group-hover:scale-105 transition-transform duration-500 ease-in-out"
+          className="transition-transform duration-500 ease-in-out group-hover:scale-105"
         />
-        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-2-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
+        <div className="max-2-[280px] absolute bottom-28 right-28 flex h-12 items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI Side</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4">
             R$129
           </span>
@@ -25,18 +25,18 @@ export default function Home() {
       </Link>
       <Link
         href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+        className="group relative col-span-3 row-span-3 flex items-center justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
-          src={'/moletom-java.png'}
+          src={"/moletom-java.png"}
           width={860}
           height={860}
           quality={100}
           alt=""
-          className="group-hover:scale-105 transition-transform duration-500 ease-in-out"
+          className="transition-transform duration-500 ease-in-out group-hover:scale-105"
         />
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-2-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
+        <div className="max-2-[280px] absolute bottom-10 right-10 flex h-12 items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI Side</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4">
             R$129
           </span>
@@ -44,18 +44,18 @@ export default function Home() {
       </Link>
       <Link
         href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+        className="group relative col-span-3 row-span-3 flex items-center justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
-          src={'/camiseta-dowhile-2022.png'}
+          src={"/camiseta-dowhile-2022.png"}
           width={860}
           height={860}
           quality={100}
           alt=""
-          className="group-hover:scale-105 transition-transform duration-500 ease-in-out"
+          className="transition-transform duration-500 ease-in-out group-hover:scale-105"
         />
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-2-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
+        <div className="max-2-[280px] absolute bottom-10 right-10 flex h-12 items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI Side</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4">
             R$129
           </span>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import { Search, ShoppingBag } from 'lucide-react';
-import Image from 'next/image';
+import Link from "next/link";
+import { Search, ShoppingBag } from "lucide-react";
+import Image from "next/image";
 
 export function Header() {
   return (
@@ -9,8 +9,8 @@ export function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           Dev Store
         </Link>
-        <form className="flex items-center gap-3 w-[320px] rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="w-5 h-5" />
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+          <Search className="h-5 w-5" />
           <input
             placeholder="Buscar produtos..."
             className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
@@ -22,11 +22,11 @@ export function Header() {
           <ShoppingBag className="h-4 w-4" />
           <span className="text-sm">Cart (0)</span>
         </div>
-        <div className="w-px h-4 bg-zinc-700" />
+        <div className="h-4 w-px bg-zinc-700" />
         <Link href="/" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Account</span>
           <Image
-            src={'https://github.com/FPPaschoal.png'}
+            src={"https://github.com/FPPaschoal.png"}
             className="h-6 w-6 rounded-full"
             alt=""
             width={24}
